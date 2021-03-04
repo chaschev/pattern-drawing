de73275be16210c67b69b5fe65ae39807e03a024
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-03-04 12:53:53 +0300

    Added buttons container panel

--- Patterns/Patterns.cs
+++ Patterns/Patterns.cs
@@ -1,30 +1,98 @@
-﻿using System;
-using cAlgo.API;
-using cAlgo.API.Internals;
-using cAlgo.API.Indicators;
-using cAlgo.Indicators;
+﻿using cAlgo.API;
+using cAlgo.Helpers;
+using System;
 
 namespace cAlgo
 {
-    [Indicator(IsOverlay = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    /// <summary>
+    /// This indicator allows you to draw chart patterns
+    /// </summary>
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class Patterns : Indicator
     {
-        [Parameter(DefaultValue = 0.0)]
-        public double Parameter { get; set; }
+        private StackPanel _panel;
 
-        [Output("Main")]
-        public IndicatorDataSeries Result { get; set; }
+        [Parameter("Orientation", DefaultValue = Orientation.Vertical, Group = "Container Panel")]
+        public Orientation PanelOrientation { get; set; }
 
+        [Parameter("Horizontal Alignment", DefaultValue = HorizontalAlignment.Left, Group = "Container Panel")]
+        public HorizontalAlignment PanelHorizontalAlignment { get; set; }
+
+        [Parameter("Vertical Alignment", DefaultValue = VerticalAlignment.Top, Group = "Container Panel")]
+        public VerticalAlignment PanelVerticalAlignment { get; set; }
+
+        [Parameter("Background Color", DefaultValue = "Gray", Group = "Container Panel")]
+        public string PanelBackgroundColor { get; set; }
+
+        [Parameter("Opacity", DefaultValue = 1, MinValue = 0, MaxValue = 1, Group = "Container Panel")]
+        public double PanelOpacity { get; set; }
+
+        [Parameter("Margin", DefaultValue = 3, Group = "Container Panel")]
+        public double PanelMargin { get; set; }
+
+        [Parameter("Background Disable Color", DefaultValue = "#FFCCCCCC", Group = "Buttons")]
+        public string ButtonsBackgroundDisableColor { get; set; }
+
+        [Parameter("Background Disable Color", DefaultValue = "Red", Group = "Buttons")]
+        public string ButtonsBackgroundEnableColor { get; set; }
+
+        [Parameter("Margin", DefaultValue = 1, Group = "Buttons")]
+        public double ButtonsMargin { get; set; }
+
+        [Parameter("Width", DefaultValue = 24, Group = "Buttons")]
+        public double ButtonsWidth { get; set; }
+
+        [Parameter("Height", DefaultValue = 24, Group = "Buttons")]
+        public double ButtonsHeight { get; set; }
 
         protected override void Initialize()
         {
-            // Initialize and create nested indicators
+            _panel = new StackPanel
+            {
+                HorizontalAlignment = PanelHorizontalAlignment,
+                VerticalAlignment = PanelVerticalAlignment,
+                Orientation = PanelOrientation,
+                BackgroundColor = ColorParser.Parse(PanelBackgroundColor),
+                Opacity = PanelOpacity,
+                Margin = PanelMargin,
+            };
+
+            _panel.AddChild(GetButton(Properties.Resources.TriangleIcon, OnClick));
+            _panel.AddChild(GetButton(Properties.Resources.TriangleIcon, OnClick));
+            _panel.AddChild(GetButton(Properties.Resources.TriangleIcon, OnClick));
+            _panel.AddChild(GetButton(Properties.Resources.TriangleIcon, OnClick));
+            _panel.AddChild(GetButton(Properties.Resources.TriangleIcon, OnClick));
+            _panel.AddChild(GetButton(Properties.Resources.TriangleIcon, OnClick));
+
+            Chart.AddControl(_panel);
         }
 
         public override void Calculate(int index)
         {
-            // Calculate value at specified index
-            // Result[index] = ...
+        }
+
+        private void OnClick(Button button, ButtonClickEventArgs args)
+        {
+            button.BackgroundColor = ColorParser.Parse(ButtonsBackgroundEnableColor);
+        }
+
+        private Button GetButton(object icon, Action<Button, ButtonClickEventArgs> onClick)
+        {
+            var button = new Button
+            {
+                Content = new Image
+                {
+                    Source = icon,
+                    Width = ButtonsWidth,
+                    Height = ButtonsHeight
+                },
+                Margin = ButtonsMargin,
+                BackgroundColor = ColorParser.Parse(ButtonsBackgroundDisableColor),
+            };
+
+            button.Click += args => onClick(button, args);
+
+            return button;
         }
     }
 }
